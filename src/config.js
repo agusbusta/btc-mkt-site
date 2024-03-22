0107390f253196f9bd1c2baa702738e2a8391c43
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-03-22 09:54:45 -0300

    backend route

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 const BASE_URL_API = process.env.REACT_APP_API_URL;
 //config server
 const config = {
-   BASE_URL:  BASE_URL_API
+   BASE_URL:  'https://aialpha.ngrok.io'
 }
 
 export default config
+   
